e54f6c3588b70af0630c7a6367b0fc1928c37ca1
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-09 21:22:54 +0530

    Add files via upload

---
+++ if -else(Decision Making).c
@@ -0,0 +1,15 @@
+class Solution { 
+    public: 
+	string compareNM(int n, int m){ 
+	// code here 
+	if ( n < m ) { 
+	return "lesser" ; 
+	} 
+	else if ( n == m ) { 
+	return "equal" ; 
+	} 
+	else {
+	return "greater" ; 
+	} 
+	}
+	};
