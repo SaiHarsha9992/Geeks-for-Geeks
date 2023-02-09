5b50e370beb44cc2d0bf7a60d546764821f63a85
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-09 21:23:53 +0530

    Add files via upload

---
+++ Vowel or not.c
@@ -0,0 +1,12 @@
+class Solution{ 
+   public: 
+   string isVowel(char c){ 
+   // code here 
+   if ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ) { 
+   return "YES" ; 
+   } 
+   else { 
+   return "NO" ; 
+   } 
+   }
+   };
