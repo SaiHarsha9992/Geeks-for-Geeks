13d1d0310d6d5c5495df3ef3960affa91004254f
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-28 21:07:03 +0530

    Add files via upload

---
+++ Remove Spaces.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        string h = "" ;
+        for ( int i = 0 ; i < s . size (  ) ; i++ )
+        {
+            if ( s [ i ] == ' ' ) 
+            {
+                continue ;
+            }
+            else
+            {
+                h . push_back ( s [ i ] ) ;
+            }
+        }
+        return h ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
