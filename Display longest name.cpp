b97b48d289eeb7813b21b369a6a16324393708be
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-28 21:06:35 +0530

    Add files via upload

---
+++ Display longest name.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        int maxLength = 0;
+        int store = 0;
+        for (int i = 0; i < n; i++) {
+            
+            if ( names [ i ] . length ( ) > maxLength )
+            {
+                maxLength = names [ i ] . length ( ) ;
+                store = i ;
+            }
+        }
+        return names [ store ] ;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
