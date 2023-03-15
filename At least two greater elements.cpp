befd11f6920e2462ed3132008ec8795ebdca9426
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-15 22:01:43 +0530

    Add files via upload

---
+++ At least two greater elements.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findElements(int a[], int n)
+    {
+        // Your code goes here   
+        vector <int> ans;
+
+        vector <int> vec;
+
+        for(int i = 0 ; i < n ; i++ )
+
+        {
+
+            ans . push_back ( a [ i ] );
+
+        }
+
+        sort ( ans . begin ( ) , ans . end ( ) ) ;
+
+        for( int i = 0 ; i< n - 2 ; i++ )
+
+        {
+
+            vec . push_back ( ans [ i ] ) ;
+
+        }
+
+        return vec;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        Solution ob;
+        vector <int> res = ob.findElements(a,n);
+        
+        for(int i=0;i<res.size();i++)
+        cout<<res[i]<<" ";
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
