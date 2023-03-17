4a0468ddc72a60471124771c6bffd2def9a104ad
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-17 20:13:05 +0530

    Add files via upload

---
+++ Binary String.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to count the number of substrings that start and end with 1.
+    long binarySubstring(int n, string str){
+        
+        // Your code here
+        
+        long a = 0, c = 0;
+        
+        for(int i=0; i<n; i++){
+            
+            if(str[i] == '1'){
+                a += c;
+                c++;
+            }
+        }
+        
+        return  a ;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n,count;
+    string a;
+    cin>>t;
+    while(t--)
+    {
+        count = 0;
+        cin >> n;
+        cin >> a;
+        Solution obj;
+        cout << obj.binarySubstring(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
