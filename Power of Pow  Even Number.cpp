6f82d27fcf8745c8212126f985198753e1e8053b
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-24 19:35:03 +0530

    Add files via upload

---
+++ Power of Pow  Even Number.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int sum_of_square_evenNumbers(long long int n)
+		{
+		    // Code here
+		    long long int s = 0 ;
+		    for ( int i = 1 ; i <= n ; i++)
+		    {
+		        s = s + pow(2*i,2);
+		    }
+		    return s ;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.sum_of_square_evenNumbers(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
