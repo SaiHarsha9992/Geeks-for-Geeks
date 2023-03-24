ca17442a34c7dcbce4216ba50a91c0dad68697dd
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-24 19:33:31 +0530

    Add files via upload

---
+++ Average in a stream.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:	
+	vector<float> streamAvg(int arr[], int n) {
+	    // code here
+	    int j = 1 ;
+	    vector <float> v ;
+	    float s = 0 ;
+	    for ( int i = 0 ; i < n ; i++ )
+	    {
+	        s = s + arr [ i ] ;
+	        v.push_back(s/j);
+	        j++;
+	    }
+	    return v ;
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.streamAvg(arr, n);
+        cout << fixed<< setprecision(2);
+        for (auto x : ans) {
+            cout <<x<<" ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
