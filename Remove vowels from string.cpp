ca45d55bd21f5496a999c789e02566a664f5c661
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-26 21:48:19 +0530

    Add files via upload

---
+++ Remove vowels from string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeVowels(string S) 
+	{
+	    // Your code goes here
+	    string s ;
+	    for ( int i = 0 ; i < S . size ( ) ; i++ )
+	    {
+	        if ( S[i] == 'a' || S[i] == 'e' || S[i] == 'i' || S[i] == 'o' || S[i] == 'u' )
+	        {
+	            continue;
+	        }
+	        else
+	        {
+	            s.push_back(S[i]);
+	        }
+	    }
+	    return s ;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s, ch; 
+
+	    getline(cin, s);  
+   		
+       
+   		Solution ob;
+   		cout << ob.removeVowels(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
