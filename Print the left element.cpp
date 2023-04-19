82047cbe2f57d904b1a942f4c52c2ac0fd9cf647
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-19 21:54:00 +0530

    Add files via upload

---
+++ Print the left element.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int leftElement(int a[], int n) {
+        // Your code goes here 
+        sort(a,a+n);
+        int i = (int) n / 2 ;
+        if ( n % 2 != 0 ){
+            return a[i];
+        }
+        else
+        {
+            return a[i-1];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.leftElement(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
