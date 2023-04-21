c6ded8e1cd6c6cee0d82f77d9951c7362e8ed3a3
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-21 19:48:30 +0530

    Add files via upload

---
+++ Area of Rectange, Right Angled Triangle and Circle.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> getAreas(int L , int W , int B , int H , int R) {
+        // code here
+        vector <int> v;
+        int ar,at,ac;
+        ar=L*W;
+        at=0.5*B*H;
+        ac=3.14*pow(R,2);
+        v.push_back(ar);
+        v.push_back(at);
+        v.push_back(ac);
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int L,W,B,H,R;
+        
+        cin>>L>>W>>B>>H>>R;
+        
+        Solution ob;
+        vector<int> ans = ob.getAreas(L,W,B,H,R);
+        cout<<ans[0]<<" "<<ans[1]<<" "<<ans[2]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
