507441f2987bfb26fbe7602a4e22839ec9ee7535
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-06 20:46:27 +0530

    Add files via upload

---
+++ Maximizearrofanarray.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.Maximize(a,n));
+        }
+        
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    
+    int Maximize(int arr[], int n)
+    {
+        // Complete the function
+        Arrays.sort(arr);
+        long sum = 0 ;
+        long m = 1000000007;
+        for (int i = 0 ; i < n ; i++){
+            long mul=(long)arr[i]*i;
+            sum = (sum + mul)%m ;
+        }
+        return (int)sum;
+        
+    }   
+}
+
