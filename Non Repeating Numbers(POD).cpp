b11280cc871c9bae385dd0f3f8904d784b8700cd
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-14 20:38:01 +0530

    Add files via upload

---
+++ Non Repeating Numbers(POD).cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        vector <int> v;
+        sort(nums.begin(),nums.end());
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1]){
+                v.push_back(nums[i]);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
