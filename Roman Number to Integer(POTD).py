de792e602d98d505b20a13c83e418a6fb114b01c
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-10-04 21:41:26 +0530

    Add files via upload

---
+++ Roman Number to Integer(POTD).py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+    def romanToDecimal(self, s): 
+        # code here
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        ans = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+        
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t = int(input())
+    for _ in range(t):
+        ob = Solution()
+        S = input()
+        print(ob.romanToDecimal(S))
+# } Driver Code Ends
