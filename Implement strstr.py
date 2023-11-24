94cf1e05486261bbc780020bc132321da867b7b5
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-11-24 21:55:39 +0530

    Add files via upload

---
+++ Implement strstr.py
@@ -0,0 +1,38 @@
+#Function to locate the occurrence of the string x in the string s.
+def strstr(s,x):
+    #code here
+    l=len(x)
+    if s=="" or x=="":
+        return -1
+    for i in range(len(s)):
+        if s[i:i+l]==x:
+            return i
+    return -1 
+
+
+#{ 
+ # Driver Code Starts
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        s,p =input().strip().split()
+        print(strstr(s,p))
+
+
+# } Driver Code Ends
