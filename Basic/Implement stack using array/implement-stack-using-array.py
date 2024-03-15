135c6c7335b9bfacbc6ccbfa3c9faabad93cb6ce
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-03-15 23:00:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement stack using array/implement-stack-using-array.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        #add code here
+        return self.arr.append(data)
+    
+    #Function to remove an item from top of the stack.
+    def pop(self):
+        #add code herearr.pop
+        if len(self.arr)==0:
+            return -1
+        else:
+            return self.arr.pop()
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyStack()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+# } Driver Code Ends
