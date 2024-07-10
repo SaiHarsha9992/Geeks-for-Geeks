8791a3c05626d3ffebab9028e3eadf1d68ffa62f
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-07-10 20:55:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Twice Counter/twice-counter.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        int countWords(string list[], int n)
+        {
+           //code here.
+           map<string, int>mp;
+           for(int i = 0; i < n; i++){
+               mp[list[i]]++;
+           }
+           int c = 0;
+           for(auto it:mp){
+               if(it.second == 2){
+                   c++;
+               }
+           }
+           return c;
+        }
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string list[n];
+        for(int i=0;i<n;i++)
+            cin>>list[i];
+        Solution ob;    
+        cout <<ob.countWords(list, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
