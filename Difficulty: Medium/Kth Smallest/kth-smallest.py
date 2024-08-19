5960021b23d71d5cf88f8df57c6a36b963177541
Gadu Kedarinadh Sai Harsha <119718764+SaiHarsha9992@users.noreply.github.com>
2024-08-19 10:30:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+
+class Solution:
+
+    def kthSmallest(self, arr,k):
+        
+        arr.sort()
+        
+        return arr[k-1]
+        
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    import random
+    t = int(input())
+    for tcs in range(t):
+        # n = int(input())
+        arr = list(map(int, input().strip().split()))
+        k = int(input())
+        ob = Solution()
+        print(ob.kthSmallest(arr, k))
+
+# } Driver Code Ends
