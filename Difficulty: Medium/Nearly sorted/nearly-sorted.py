93ff72feefbd7c578fbc74e6bb612cb51e5db32a
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-11-14 21:50:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def nearlySorted(self, arr, k):
+        #code
+        arr.sort()
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+# Position this line where user code will be pasted.
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        k = int(input())
+        ob = Solution()
+        ob.nearlySorted(arr, k)
+        print(*arr)
+        # print("~")
+        t -= 1
+# } Driver Code Ends
