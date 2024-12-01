70a98fe0190f2c16466fa415b389e98e73cbb0e3
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-12-01 21:24:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.py
@@ -0,0 +1,57 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first non-repeating character in a string.
+    def nonRepeatingChar(self,s):
+        #code here
+        dic = {}
+        for i in s:
+            if i in dic:
+                dic[i]+=1
+                
+            else:
+                dic[i]= 1
+                
+        for i in s:
+            if (dic[i]==1):
+                return i
+                
+        return -1
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        ans = obj.nonRepeatingChar(s)
+        if (ans != '$'):
+            print(ans)
+        else:
+            print(-1)
+
+        print("~")
+
+# } Driver Code Ends
