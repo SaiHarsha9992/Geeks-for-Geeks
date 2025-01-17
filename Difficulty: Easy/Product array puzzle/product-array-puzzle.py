b3bfd9c5630467f5a67210f72f95eac3d9d08c44
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2025-01-17 21:45:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.py
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -1,16 +1,16 @@
+#User function Template for python3
+
 class Solution:
     def productExceptSelf(self, nums):
         #code here
         n = len(nums)
-        res = [1] * n  # Initialize the result array with 1s
-        
-        # Calculate the left product and store it in res
+        res = [1] * n  
+
         left_product = 1
         for i in range(n):
             res[i] = left_product
             left_product *= nums[i]
         
-        # Calculate the right product and multiply it with the res
         right_product = 1
         for i in range(n-1, -1, -1):
             res[i] *= right_product
@@ -26,10 +26,11 @@ if __name__ == '__main__':
     t = int(input())
 
     for _ in range(t):
-        n = int(input())
+
         arr = [int(x) for x in input().split()]
 
         ans = Solution().productExceptSelf(arr)
         print(*ans)
+        print("~")
 
 # } Driver Code Ends
