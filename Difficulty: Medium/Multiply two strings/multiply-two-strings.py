b6a9f3202d2362572faa2ce2af5b646964e43bd7
Kedarinadh Sai Harsha Gadu <gaduharsha72@gmail.com>
2025-04-27 22:03:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        # code here
+        # return the product string
+        return int(s1) * int(s2)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        a = input()
+        b = input()
+        print(Solution().multiplyStrings(a.strip(), b.strip()))
+
+        print("~")
+
+# } Driver Code Ends
