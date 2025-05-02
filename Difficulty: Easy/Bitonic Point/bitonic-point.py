a925bbed084d8626b426ae883506052b6b0e1ca7
Kedarinadh Sai Harsha Gadu <gaduharsha72@gmail.com>
2025-05-02 23:20:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bitonic Point/bitonic-point.py
+++ Difficulty: Easy/Bitonic Point/bitonic-point.py
@@ -1,27 +1,25 @@
 #User function Template for python3
 class Solution:
 
-	def findMaximum(self,arr, n):
+	def findMaximum(self, arr):
 		# code here
-		return max(arr)
+		for i in range(1,len(arr)-1):
+            if arr[i]>arr[i+1] and arr[i] > arr[i-1]:
+                return arr[i]
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-
-
-
-
 if __name__ == '__main__':
     tc = int(input())
     while tc > 0:
-        n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ans = ob.findMaximum(arr, n)
+        ans = ob.findMaximum(arr)
         print(ans)
         tc -= 1
+        print("~")
 
 # } Driver Code Ends
